f5f119b4a8bb4c1c050e81a9bf24ad5c967ab106
Joshua Stresing <pyrotorch69@yahoo.com>
2022-02-09 16:48:07 -0800

    im done

--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,18 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
-
+import { renderGoblin } from '../utils.js';
 const test = QUnit.test;
 
 test('time to test a function', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = '<div class="goblin"><p>Booger</p><img id="goblin-face-1" src="./assets/goblin.png"><p id="goblin-hp-1">1</p></div>';
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = renderGoblin ({ id: 1, name: 'Booger', hp: 1 });
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+    expect.equal(actual.outerHTML, expected);
 });
